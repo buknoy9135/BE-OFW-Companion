c135226b2b3add7d76467f805815a4db5c72e412
buknoy9135 <jalilanthony@gmail.com>
2025-09-17 01:20:50 +0800

    try plain instead of login in mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,20 @@ Rails.application.configure do
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: "https://ofw-companion.onrender.com", protocol: "https" }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp-relay.brevo.com",
+    port:                 587,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
